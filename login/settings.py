874d0b71695c380d4a31ab4f6ef9aea368c76f6b
lennney <lennney@example.com>
2026-04-13 05:56:03 +0800

    Configure PostgreSQL for Railway deployment with dj-database-url

--- login/settings.py
+++ login/settings.py
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
 }
 
 
